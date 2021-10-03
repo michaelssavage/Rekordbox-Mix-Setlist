cb57e9c9b80d355638a87b3bd9366c01bea2b235
savagem7 <i0g1n4oj>
2021-10-03 17:16:03 +0100

    readme update and error handling

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # Rekordbox-Mix-Setlist
-Really simple executable that I used to get a setlist txt file from Rekordox.
+Really simple executable used to get a setlist .txt file from Rekordbox.
+Executable created using pyinstaller.
+
+## Steps
+1. Export playlist to a file for Kuvo.
+
+<img src="img/export.jpg" alt="exporting" width="720" />
+
+2. Make sure the executable is in the same location as the playlist.
+
+<img src="img/executable.jpg" alt="executable file" width="600" />
+
+3. Double-click the executable and enter the text file name.
+
+<img src="img/example1.jpg" alt="example without .txt extension" width="600" />
+<img src="img/example2.jpg" alt="example with .txt extension" width="600" />
+
+## Errors
+
+Files that don't exist will print out possible .txt options.
+
+<img src="img/error.jpg" alt="error file name" width="600" />
--- setlist.py
+++ setlist.py
@@ -1,33 +1,36 @@
 import os 
 import re
 
-directory = r"C:\Users\micha\Documents\Mixes"
-f = input("Enter the name of the file: ")
+def main():
+    path = os.getcwd()
+    f = input("Enter the name of the file: ")
 
-if f[-4:] != '.txt':
-    filename = f"{f}.txt"
+    filename = f"{f}.txt" if f[-4:] != '.txt' else f
 
-if os.getcwd() != directory:
-    os.chdir(directory)
+    try:
+        updated_content = ""
 
-try:
-    updated_content =""
+        for line in open(filename, 'r', encoding='utf-16'):
+            songInfo = line.strip().split("\t")
+            song = f"{songInfo[3].strip()} - {songInfo[4].strip()}\n"
+            updated_content += song
 
-    for line in open(filename, 'r', encoding='utf-16'):
-        songInfo = line.strip().split("\t")
-        song = f"{songInfo[3].strip()} - {songInfo[4].strip()}\n"
-        updated_content += song
+        with open(filename,'w', encoding='utf-16') as file:
+            file.write(updated_content)
+        print("File updated!")
 
-    with open(filename,'w', encoding='utf-16') as file:
-        file.write(updated_content)
-    
-    print("File updated!")
-except FileNotFoundError:
-    print("==========")
-    print(f"File not found: {f}. Did you spell it correctly?")
-    print(f"Possible filenames: ")
-    for i,file in enumerate(os.listdir(directory)):
-        if file.endswith('.txt'):
-            print(f"\t{i+1}. {file}")
+    except FileNotFoundError:
+        print("==========")
+        print(f"'{f}' file not found. Did you spell it correctly?")
+        print(f"Possible filenames: ")
+        for file in os.listdir(path):
+            if file.endswith('.txt'):
+                print(f"\t• {file}")
+    except IndexError:
+        print("==========")
+        print(f"'{f}' file format does not match Kuvo output. Did you export from Rekordbox properly?")
 
-k=input("press any buton to exit")
+    k=input("press any button to exit.")
+
+if __name__ == "__main__":
+    main()
