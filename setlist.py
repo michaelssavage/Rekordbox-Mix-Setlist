a76f92dfe3ad10a0bb3b35496c6cc6fe572206b5
savagem7 <i0g1n4oj>
2021-10-05 21:27:27 +0100

    choice given to add BPM. Numbered list outputted to new file now.

--- setlist.py
+++ setlist.py
@@ -1,24 +1,43 @@
 import os 
 import re
 
+def getBPMChoice():
+    loop = True
+    choice = False
+    while loop:
+        c = input("Do you want to include the BPM? enter Y or N: ")
+        if c.upper() == "Y":
+            choice = True
+            loop = False
+        elif c.upper() == "N":
+            loop = False
+        else:
+            print("Please enter Y or N only.")
+    return choice
+
 def main():
     path = os.getcwd()
     f = input("Enter the name of the file: ")
 
+    choice = getBPMChoice()
     filename = f"{f}.txt" if f[-4:] != '.txt' else f
 
     try:
         updated_content = ""
 
-        file = open(filename, 'r', encoding='utf-16')
-        fileInfo = file.readlines()
+        fileInfo = open(filename, 'r', encoding='utf-16').readlines()
         songInfo = fileInfo[0].strip().split("\t")
         artist = songInfo.index("Artist")
         track = songInfo.index('Track Title')
+        bpm = songInfo.index('BPM')
 
-        for line in fileInfo[1:]:
+        for i, line in enumerate(fileInfo[1:]):
             songInfo = line.strip().split("\t")
-            song = f"{songInfo[artist].strip()} - {songInfo[track].strip()}\n"
+
+            song = f"{i+1}. {songInfo[artist].strip()} - {songInfo[track].strip()}\n"
+            if choice:
+                song = song[:-2] + f" ({songInfo[bpm].strip()})\n"
+
             updated_content += song
 
         with open("_"+filename,'w', encoding='utf-16') as file:
